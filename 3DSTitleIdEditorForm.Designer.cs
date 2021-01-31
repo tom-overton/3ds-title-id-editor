c4c025c7670e15aad64772301848bdac9d562095
tom-overton <tom.overton@outlook.com>
2021-01-30 19:04:38 -0800

    First commit with code

---
+++ 3DSTitleIdEditorForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _3DSTitleIdEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.loadGameImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.overwriteGameImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.titleIdTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.toolStrip.SuspendLayout();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(256, 25);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadGameImageToolStripMenuItem,
+            this.overwriteGameImageToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // loadGameImageToolStripMenuItem
+            // 
+            this.loadGameImageToolStripMenuItem.Name = "loadGameImageToolStripMenuItem";
+            this.loadGameImageToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.loadGameImageToolStripMenuItem.Text = "Load game image";
+            this.loadGameImageToolStripMenuItem.Click += new System.EventHandler(this.loadGameImageToolStripMenuItem_Click);
+            // 
+            // overwriteGameImageToolStripMenuItem
+            // 
+            this.overwriteGameImageToolStripMenuItem.Enabled = false;
+            this.overwriteGameImageToolStripMenuItem.Name = "overwriteGameImageToolStripMenuItem";
+            this.overwriteGameImageToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.overwriteGameImageToolStripMenuItem.Text = "Overwrite game image";
+            this.overwriteGameImageToolStripMenuItem.Click += new System.EventHandler(this.overwriteGameImageToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // titleIdTextBox
+            // 
+            this.titleIdTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleIdTextBox.Location = new System.Drawing.Point(15, 19);
+            this.titleIdTextBox.Name = "titleIdTextBox";
+            this.titleIdTextBox.Size = new System.Drawing.Size(211, 20);
+            this.titleIdTextBox.TabIndex = 1;
+            this.titleIdTextBox.TextChanged += new System.EventHandler(this.titleIdTextBox_TextChanged);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.titleIdTextBox);
+            this.groupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox.Location = new System.Drawing.Point(12, 28);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(232, 55);
+            this.groupBox.TabIndex = 2;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Title ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 95);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.toolStrip);
+            this.Name = "Form1";
+            this.Text = "3DS Title ID Editor";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem loadGameImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem overwriteGameImageToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox titleIdTextBox;
+        private System.Windows.Forms.GroupBox groupBox;
+    }
+}
+
---
+++ 3DSTitleIdEditorForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _3DSTitleIdEditor
+{
+    public partial class Form1 : Form
+    {
+        private string filename;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadGameImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.filename = openFileDialog.FileName;
+                byte[] fileType = this.GetFileType();
+                int startingOffset = -1;
+                if (this.IsFileTypeNCSD(fileType))
+                {
+                    startingOffset = 0x4000;
+                }
+                else if (this.IsFileTypeNCCH(fileType))
+                {
+                    startingOffset = 0x0;
+                }
+                else
+                {
+                    throw new ArgumentException("Supplied game image is not an NCSD or NCCH.");
+                }
+
+                string titleId = this.ReadTitleIdFromGameImage(startingOffset);
+                titleIdTextBox.Text = titleId;
+                overwriteGameImageToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private byte[] GetFileType()
+        {
+            byte[] buffer = new byte[4];
+            using (FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Read))
+            {
+                fs.Seek(0x100, SeekOrigin.Begin);
+                fs.Read(buffer, 0, buffer.Length);
+            }
+
+            return buffer;
+        }
+
+        private bool IsFileTypeNCSD(byte[] fileType)
+        {
+            return fileType[0] == 0x4E && fileType[1] == 0x43 && fileType[2] == 0x53 && fileType[3] == 0x44;
+        }
+
+        private bool IsFileTypeNCCH(byte[] fileType)
+        {
+            return fileType[0] == 0x4E && fileType[1] == 0x43 && fileType[2] == 0x43 && fileType[3] == 0x48;
+        }
+
+        private string ReadTitleIdFromGameImage(int startingOffset)
+        {
+            byte[] programCode = new byte[0x8];
+            using (FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Read))
+            {
+                fs.Seek(startingOffset + 0x118, SeekOrigin.Begin);
+                fs.Read(programCode, 0, programCode.Length);
+            }
+
+            Array.Reverse(programCode);
+            return BitConverter.ToString(programCode).Replace("-", String.Empty);
+        }
+
+        private void overwriteGameImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string titleId = titleIdTextBox.Text;
+            byte[] titleIdBytes = new byte[titleId.Length / 2];
+            for (int i = 0; i < titleId.Length; i += 2)
+            {
+                titleIdBytes[i / 2] = Convert.ToByte(titleId.Substring(i, 2), 16);
+            }
+
+            Array.Reverse(titleIdBytes);
+            byte[] fileType = this.GetFileType();
+            int startingOffset = -1;
+            if (this.IsFileTypeNCSD(fileType))
+            {
+                startingOffset = 0x4000;
+            }
+            else if (this.IsFileTypeNCCH(fileType))
+            {
+                startingOffset = 0x0;
+            }
+            else
+            {
+                throw new ArgumentException("Supplied game image is not an NCSD or NCCH.");
+            }
+            this.WriteTitleIdToGameImage(titleIdBytes, startingOffset);
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            MessageBox.Show("Successfully saved the new title ID!", "", buttons);
+        }
+
+        private void WriteTitleIdToGameImage(byte[] titleIdBytes, int startingOffset)
+        {
+            using (FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Write))
+            {
+                fs.Seek(startingOffset + 0x118, SeekOrigin.Begin);
+                fs.Write(titleIdBytes, 0, titleIdBytes.Length);
+            }
+        }
+
+        private void titleIdTextBox_TextChanged(object sender, EventArgs e)
+        {
+            bool valid = true;
+            if (titleIdTextBox.Text.Length != 16)
+            {
+                valid = false;
+            }
+            else
+            {
+                long unused;
+                if (!long.TryParse(titleIdTextBox.Text, System.Globalization.NumberStyles.HexNumber, null, out unused))
+                {
+                    valid = false;
+                }
+            }
+
+            if (valid)
+            {
+                titleIdTextBox.BackColor = System.Drawing.Color.White;
+                titleIdTextBox.ForeColor = System.Drawing.Color.Black;
+                overwriteGameImageToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                titleIdTextBox.BackColor = System.Drawing.Color.Red;
+                titleIdTextBox.ForeColor = System.Drawing.Color.White;
+                overwriteGameImageToolStripMenuItem.Enabled = false;
+            }
+        }
+    }
+}
